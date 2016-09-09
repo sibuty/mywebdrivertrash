16dd84c9975df7048309c3ebdf9aeab497d389aa
Igor Goryainov <igor.goryainov@altarix.ru>
2016-09-09 12:01:58 +0400

    Add new vote.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -20,11 +20,13 @@ public class Main {
 
         for (int i = 0; i < 250; i++) {
             WebDriver driver = new ChromeDriver();
-            driver.get("http://www.wday.ru/krasota-zdorovie/beauty-gid/kak-solntse-10-samyih-obvorojitelnyih-ryijih-krasotok-samaryi-golosuy/12/");
+            //driver.get("http://www.wday.ru/krasota-zdorovie/beauty-gid/kak-solntse-10-samyih-obvorojitelnyih-ryijih-krasotok-samaryi-golosuy/12/");
+            driver.get("http://www.wday.ru/moda-shopping/style/koroleva-woman-s-day-2016-40-pobeditelnits-konkursov-krasotyi/10/");
 
             try {
                 TimeUnit.SECONDS.sleep(random.nextInt(5) + 5);
-                ((JavascriptExecutor) driver).executeScript("document.getElementById('poll4009-answer-48700').click()");
+                //((JavascriptExecutor) driver).executeScript("document.getElementById('poll4009-answer-48700').click()");
+                ((JavascriptExecutor) driver).executeScript("document.getElementById('poll6482-answer-81940').click()");
                 TimeUnit.SECONDS.sleep(random.nextInt(200) + 30);
 
             } catch (Exception e) {
