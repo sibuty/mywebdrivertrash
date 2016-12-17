8824dfafe16c45ab87bb1c7cd7e31b30900ac384
Igor Goryainov <igor.goryainov@altarix.ru>
2016-12-17 19:50:53 +0400

    Added chromedrive at windows.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,9 @@
 package main;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.IOUtils;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
+import java.io.*;
 
 /**
  * Created by glotemz on 16.10.15.
@@ -13,13 +11,31 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver",
-                "/home/glotemz/projects/myhackervote/src/main/resources/chromedriver");
+        initChromeDriverWindows();
 
         SchoolHelper schoolHelper = new SchoolHelper(new ChromeDriver());
 
         String input = System.console().readLine();
-
         schoolHelper.selectTown(Integer.parseInt(input));
+    }
+
+    public static void initChromeDriverWindows() {
+        InputStream in = Main.class.getClassLoader().getResourceAsStream("chromedriver.exe");
+        File file = new File("chromedriver.exe");
+        OutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            IOUtils.copy(in, out);
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
     }
 }
